14f2ad5979ec9001b9db2ebe93d9f37da32ba308
izacoding <dsteele008@gmail.com>
2018-06-21 13:13:12 -0400

    Libaray

---
+++ script.js
@@ -0,0 +1,49 @@
+function (){
+	var books[
+
+
+{Title:"The Catcher And The Rye",
+
+Author:"J. D. Salinger",
+
+Published:1951,
+
+Pages:277,
+
+readingStatus:True}
+
+
+ {Title:"Othello",
+
+Author:"William Shakesphere",
+
+Published:2003,
+
+Pages:368,
+
+readingStatus:True}
+
+
+{Title:"The Strangers",
+
+Author:"Albert Camus",
+
+Published:1942,
+
+Pages:134,
+
+readingStatus:True}
+
+
+{Title:"The Color Purple",
+
+Author:"Alice Walker",
+
+Published:1982,
+
+Pages:347
+
+readingStatus:False}
+};
+ ];
+   };
