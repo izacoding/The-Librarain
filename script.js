60000c81774f1f9db6faa10741015b9f53f11d63
izacoding <dsteele008@gmail.com>
2018-06-21 16:24:19 -0400

    added new elements

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
-function (){
-	var books[
+
+document.getElementById("addBook").addEventListener("click","function");
 
 
-{Title:"The Catcher And The Rye",
 
-Author:"J. D. Salinger",
-
-Published:1951,
-
-Pages:277,
-
-readingStatus:True}
-
-
- {Title:"Othello",
-
-Author:"William Shakesphere",
-
-Published:2003,
-
-Pages:368,
-
-readingStatus:True}
-
-
-{Title:"The Strangers",
-
-Author:"Albert Camus",
-
-Published:1942,
-
-Pages:134,
-
-readingStatus:True}
-
-
-{Title:"The Color Purple",
-
-Author:"Alice Walker",
-
-Published:1982,
-
-Pages:347
-
-readingStatus:False}
+function Book(T, A, P ,Y){
+	return{
+		Title: t,
+		Author: a,
+		Pages: p,
+		Published:y,
+	}
 };
- ];
-   };
+let entry = new Book(Title, Author, Pages ,Published);
+ var Title = document.getElementById("Title").vaule();
+};
+
+  
